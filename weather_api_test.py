4f3ca050512566a177c4dd677962e00d8380b2cf
Dominic Critchlow <DOM@Dominics-MacBook-Pro.local>
2017-03-24 15:47:45 -0500

    updated weather api file. using requests.

--- weather_api_test.py
+++ weather_api_test.py
@@ -1,4 +1,13 @@
-from pprint import pprint
 import requests
-r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Clarksville&units=metric&APPID=301e59e1ab9370e1f7644445df46f156')
-pprint(r.json())
+
+url = 'http://api.openweathermap.org/data/2.5/weather?APPID=301e59e1ab9370e1f7644445df46f156' # url goes here
+location = 'Clarksville' # adjust as required
+unit = 'metric'
+paramaters = {'q':location, 'units':unit}
+response = requests.get(url, params = paramaters)
+if response.status_code == 200:
+    data = response.json()
+else:
+    print('Something went wrong')
+
+print data #data[u'weather'][0][u'description'] #adressing data from the set
