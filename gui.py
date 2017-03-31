2b2d018e0c981abf9d14e3fc65a3ecf7fe4bb4ba
Dominic Critchlow <DOM@dominics-mbp.apsu.edu>
2017-03-31 09:50:23 -0500

    Added Calender/Upcoming Events, in order to run you need the .json file and you need to pip install this line ->  pip install --upgrade google-api-python-client

--- gui.py
+++ gui.py
@@ -5,7 +5,7 @@
 # - Weather Information
 # - Most recent tweet from POTUS
 # - Greeting Daytime
-
+from __future__ import print_function
 from Tkinter import *
 import time
 import requests
@@ -13,8 +13,64 @@ import tweepy
 import datetime
 import xmltodict
 
+
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+
+SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Google Calendar API Python Quickstart'
+
+
 fonttype = 'adobe' # different maybe franklin
 fontstyle = 'bold'
+
+
+
+
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'calendar-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+
 
 ###################################################
 # Functions to be repeated and updated while running
@@ -39,7 +95,7 @@ def stockAPICall():
     if stock.status_code == 200:
         data1 = xmltodict.parse(stock.content)
 
-    print data1
+    print (data1)
     stockData.config(text = "Stock: " + data1[u'StockQuote'][u'Name']
                             +"\nStock Price: " + data1[u'StockQuote'][u'LastPrice'])
 
@@ -55,7 +111,7 @@ def weatherAPICall():
     else:
         print('Something went wrong')
 
-    print data
+    print (data)
     t = u"\u00b0"
     WeatherTemp.config(text = data[u'weather'][0][u'description']
                         + '\nTemperature: '
@@ -71,18 +127,48 @@ def twitterAPICall():
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
 
-    potus_tweet = api.user_timeline(screen_name = 'potus',count=2)
-    print potus_tweet[0].text
-    PotusTweet.config(text = potus_tweet[0].text)
+    potus_tweet = api.user_timeline(screen_name = 'cnnbrk',count=2)
+    result_tweet = potus_tweet[0].text
+    #result_tweet = result_tweet.sub(r"http\S+", "", subject)
+    print (result_tweet)
+    PotusTweet.config(text = result_tweet)
 
 def timeOfDay():
     currentTime = datetime.datetime.now()
     if currentTime.hour < 12:
-        DayGreet.config(text = 'Good Morning Handsome')
+        DayGreet.config(text = 'Good Morning \nHandsome')
     elif 12 <= currentTime.hour < 18:
         DayGreet.config(text = 'Good Afternoon \nHandsome')
     else:
-        DayGreet.config(text = 'Good Evening Handsome')
+        DayGreet.config(text = 'Good Evening \nHandsome')
+
+
+def calendarAPICall():
+    """Shows basic usage of the Google Calendar API.
+
+    Creates a Google Calendar API service object and outputs a list of the next
+    10 events on the user's calendar.
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http)
+
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print (now)
+    print('Getting the upcoming 10 events')
+    eventsResult = service.events().list(
+        calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
+        orderBy='startTime').execute()
+    events = eventsResult.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+        CalenderLabel.config(text = 'No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
+    CalenderLabel.config(text = events[0]['start'].get('dateTime', events[0]['start'].get('date'))
+    + " " + events[0]['summary'])
 
 ###################################################
 
@@ -109,7 +195,7 @@ WeatherTemp.pack(side = RIGHT, expand =0)
 #Stock Object
 stockData = Label(root, font=(fonttype,40,fontstyle),bg='black',fg= 'white')
 stockData.pack(side = RIGHT, expand =0)
-stockData.place(x=900, y=400)
+stockData.place(x=900, y=700)
 
 # Title Object
 Title = Label(root, font=(fonttype, 80, fontstyle), bg='black',fg='white')
@@ -128,8 +214,11 @@ Title.config(text = 'Project by Group 1: \nDominic Critchlow, Travis Hodge, Dyla
 # POTUS Twitter
 PotusTweet = Label(root, font=(fonttype,12,fontstyle),bg='black',fg= 'white')
 PotusTweet.pack(expand = 0)
-###################################################
 
+# Calendar Label
+###################################################
+CalenderLabel = Label(root, font=(fonttype,12,fontstyle),bg='black',fg= 'white')
+CalenderLabel.pack(expand = 0)
 
 
 
@@ -137,9 +226,9 @@ PotusTweet.pack(expand = 0)
 # Call Function for items
 clockUpdate()
 weatherAPICall()
-twitterAPICall()
 stockAPICall()
-#timeOfDay()
+twitterAPICall()
+calendarAPICall()
 ###################################################
 
 
