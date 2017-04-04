44b3d79dc01da0dd11a0378ff6a29b4fd1e49bad
MichaelTimbes <mtimbes@my.apsu.edu>
2017-04-03 22:24:42 -0500

    Removed Test.py file- serial is the new name. Added some comments for clarity.

--- serial.py
+++ serial.py
@@ -7,6 +7,8 @@ from time import sleep
 #For Unix:
 #Before testing- be sure that the serial port used in Arduino sketch uploading matches the
 #port that ser uses.
+#To do so run the following: ls /dev/tty.*
+#Then paste it as the string in the serial constructor.
 ser = serial.Serial('/dev/tty.usbmodemFD123') #this is specific to each Mac so be sure to update
 sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser)) #Needed for later writing from buff
 print(ser.name)
